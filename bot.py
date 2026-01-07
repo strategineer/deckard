b502806fcc4c261b25bc77ccdf1e20ba6a352b6d
Bill Tyros <me@strategineer.com>
2026-01-07 10:58:38 -0500

    Dice roller

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import configparser
 import sys
 
+import dice
 import discord
 
 config = configparser.ConfigParser()
@@ -21,12 +22,19 @@ client = discord.Client(intents=intents)
 async def on_ready():
     print(f'We have logged in as {client.user}')
 
+HELLO_COMMAND = '$hello'
+ROLL_COMMAND = '$roll'
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
+    if message.content.startswith(HELLO_COMMAND):
         await message.channel.send('Hello!')
+
+    if message.content.startswith(ROLL_COMMAND):
+        roll_cmd = message.content[len(ROLL_COMMAND):]
+        result = dice.roll(roll_cmd)
+        await message.channel.send(f'{roll_cmd}: {result}')
 
 client.run(BOT_TOKEN)
